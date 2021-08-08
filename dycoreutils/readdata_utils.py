aabc80184a8b408b0c622a291c7eeb22ce084972
Isla Simpson <islas@tungsten.cgd.ucar.edu>
2021-08-08 17:55:48 -0600

    adding in some extra's to deal with piControl calendar

--- dycoreutils/readdata_utils.py
+++ dycoreutils/readdata_utils.py
@@ -14,23 +14,33 @@ def read_cesm_zonalmean(filepath, datestart, dateend):
         dateend (string) = enddate for timeslice (in a normal calendar)
     """
 
-    dat = xr.open_mfdataset(filepath, coords="minimal", join="override", decode_times = True)
-
     try:
+
+        dat = xr.open_mfdataset(filepath, coords="minimal", join="override", decode_times = True)
+    
         try:
-            timebndavg = np.array(dat.time_bnds,
+            try:
+                timebndavg = np.array(dat.time_bnds,
                      dtype='datetime64[s]').view('i8').mean(axis=1).astype('datetime64[s]')
+            except:
+                timebndavg = np.array(dat.time_bounds,
+                     dtype='datetime64[s]').view('i8').mean(axis=1).astype('datetime64[s]')
+    
+            dat['time'] = timebndavg
         except:
-            timebndavg = np.array(dat.time_bounds,
-                     dtype='datetime64[s]').view('i8').mean(axis=1).astype('datetime64[s]')
+            print("warning, you're reading CESM data but there's no time_bnds")
+            print("make sure you're reading in what you're expecting to")
+    
+        dat = dat.sel(time=slice(datestart, dateend)).mean("lon")
 
-
-        dat['time'] = timebndavg
     except:
-        print("warning, you're reading CESM data but there's no time_bnds")
-        print("make sure you're reading in what you're expecting to")
-
-    dat = dat.sel(time=slice(datestart, dateend)).mean("lon")
+        dat = xr.open_mfdataset(filepath, coords="minimal", join="override")
+        timebnds = dat.time_bnds
+        diff = np.array(timebnds.isel(nbnd=1)) - np.array(timebnds.isel(nbnd=0))
+        diff = diff/2.
+        newtime = np.array(timebnds.isel(nbnd=0)) + diff
+        dat['time'] = newtime
+        dat = dat.sel(time=slice(datestart, dateend)).mean("lon")
 
     return dat
 
